77bfcdc8d33b2b1e939b1c6e3286d327c224f86e
shcho <shcho@blocko.io>
2016-06-20 13:45:13 +0900

    AbstractTest, TestStamp, TestGetStamp

---
+++ src/test/java/io/blocko/coinstack/study/AbstractTest.java
@@ -0,0 +1,36 @@
+package io.blocko.coinstack.study;
+
+import org.junit.*;
+
+import io.blocko.coinstack.CoinStackClient;
+import io.blocko.coinstack.Endpoint;
+import io.blocko.coinstack.model.CredentialsProvider;
+
+public abstract class AbstractTest {
+	
+	private CoinStackClient client = null;
+	
+	@Before
+	public void init() {
+		client = new CoinStackClient(new CredentialsProvider() {
+			@Override
+			public String getAccessKey() {
+				return "17155ccf15e603853c19a35559f3f5";
+			}
+			@Override
+			public String getSecretKey() {
+				return "4ffe022576916bf0d9c4c13718d582";
+			}
+		}, Endpoint.MAINNET);
+	}
+	
+	@After
+	public void fini() {
+		client.close();
+	}
+	
+	protected CoinStackClient getClient() {
+		return client;
+	}
+	
+}
---
+++ src/test/java/io/blocko/coinstack/study/TestStamp.java
@@ -0,0 +1,33 @@
+package io.blocko.coinstack.study;
+
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.*;
+
+import io.blocko.bitcoinj.core.Sha256Hash;
+import io.blocko.coinstack.CoinStackClient;
+import io.blocko.coinstack.exception.CoinStackException;
+import io.blocko.spongycastle.util.encoders.Hex;
+
+public class TestStamp extends AbstractTest {
+	
+	@Test
+	public void testStamp() throws IOException, CoinStackException {
+		CoinStackClient client = getClient();
+		
+		String testMessage = "HELLO_WORLD";
+		Sha256Hash hash = Sha256Hash.create(testMessage.getBytes());
+		String strHash = Hex.toHexString(hash.getBytes());
+		System.out.println("strHash: "+strHash);
+		
+		String stampId = client.stampDocument(strHash);
+		assertNotNull(stampId);
+		System.out.println("stampId: "+stampId);
+		
+		//strHash: 6f9b514093848217355d76365df1f54f42bdfd5f4e5f54a654c46b493d162c39
+		//stampId: 7e7335644971d7821d89bc64e57da4cece6ffde86c97942eb7bad145acbd258f-0
+	}
+}
